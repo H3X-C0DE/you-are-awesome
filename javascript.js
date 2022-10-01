1cb6c8aa215b8961d70209be8ae6b6352cc01f92
IIHEXYII <vetletessem@gmail.com>
2022-10-01 14:16:28 +0200

    added mobile responsive features

--- javascript.js
+++ javascript.js
@@ -10,7 +10,14 @@ function play() {
   swapBackground.classList.toggle("hidden");
 }
 
-const toggleNavbarVisibility = () => {
-  isNavbarExpanded = !isNavbarExpanded;
-  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
-};
+// copy to clipboard button
+function CopyToClip() {
+  const copyText = document.getElementById("myInput");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value);
+
+  alert("Copied Discord ID " + copyText.value + " to your clipboard!");
+}
